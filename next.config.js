205b87298215e6a021fcd2f6e48f543e6a335c66
Shivam Atre <shivamatre003@gmail.com>
2026-07-03 20:35:07 +0530

    Fix formatting and add optimizeFonts option

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'standalone',
-  swcMinify: true
+  swcMinify: true,
+  optimizeFonts: false // [!code ++]
 };
 
 module.exports = config;
